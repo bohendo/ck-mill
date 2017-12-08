904a6ff15a7c5e6fbbc6def754136d096921a515
Bo Henderson <bohende@gmail.com>
2017-12-08 11:16:12 -0800

    Add more helpful kitty syncing messages

--- src/kittysync.js
+++ src/kittysync.js
@@ -10,7 +10,7 @@ const syncKitties = () => {
     (function kittyLoop(i) {
       if (i >= max) return
       getKitty(i).then(k=>{
-        const msg = k.forsale ? `(On sale for ${Math.round(web3.utils.fromWei(k.currentprice, 'ether'), 3)})` : ''
+        const msg = k.forsale ? `(On sale for ${Math.round(web3.utils.fromWei(k.currentprice, 'milli'), 3)} mETH)` : ''
         console.log(`Got gen ${k.generation} kitty ${k.id} ${msg}`)
         kittyLoop(i+1)
       }).catch(err => { console.error(err); process.exit(1) })
