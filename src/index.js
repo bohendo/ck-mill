da6751840dc0ac791d730bbfa6c5adcd0b2b2ce5
Bo Henderson <bohende@gmail.com>
2017-12-15 20:11:17 -0800

    Initialize breedgroup tool

--- src/index.js
+++ src/index.js
@@ -1,10 +1,55 @@
 import { core, sale, sire } from './ck/'
 
-const myGen0 = [25493]
-const myGen1 = [228842, 117491, 113881, 85736, 3954]
 const myKitties = [291628, 282323, 279505, 258963, 228842, 117491, 113881, 85736, 25856, 25493, 24111, 3954]
-
 const fromBlock = 4605167
+const bg = {
+  A: [228842, 117491],
+  B: [113881, 85736, 3954],
+}
+
+const breedGroup = (lok) => {
+  let ready = []
+  for (let i=0; i<lok.length; i++) {
+    // Do you own this kitty?
+    if (eth.accounts[0] !== core.ownerOf(lok[i])) {
+      return(`Error: you don't own kitty ${lok[i]}`)
+    }
+    if (core.getKitty(lok[i])[1]) {
+      ready.push(lok[i])
+    }
+    // Can this kitty breed with the others in the group?
+    for (let j=i+1; j<lok.length; j++) {
+      if (!core.canBreedWith(lok[i], lok[j])) {
+        return(`Error: kitty ${lok[i]} and kitty ${lok[j]} can't breed`)
+      }
+    }
+  }
+
+  const kitties = []
+  for (let i=0; i<ready.length; i++) {
+    kitties.push(core.getKitty(ready[i]))
+  }
+
+  kitties.sort((a,b)=>a[2]-b[2])
+
+  console.log(`Confirm siring kitty ${ready[1]} (gen=${kitties[1][8]},cdi=${kitties[1][2]}) with matron kitty ${ready[0]} (gen=${kitties[0][8]},cdi=${kitties[0][2]})`)
+
+  // (matron, sire)
+  const tx = {
+    from: eth.accounts[0],
+    to: core.address,
+    value: core.autoBirthFee(),
+    gas: 150000,
+    gasPrice: eth.gasPrice * 0.9,
+    data: core.breedWithAuto.getData(ready[0], ready[1]),
+  }
+  console.log(JSON.stringify(tx, null, 2))
+  personal.unlockAccount(eth.accounts[0])
+  var txhash = eth.sendTransaction(tx)
+  personal.lockAccount(eth.accounts[0])
+  return eth.getTransaction(txhash)
+}
+
 
 const status = () => {
   if (eth.syncing) {
@@ -107,4 +152,4 @@ const getKitty = (id) => {
   return kitty
 }
 
-export { core, sale, sire, fromBlock, getKitty, getMyKitties, myKitties, sellKitty, status }
+ export { core, sale, sire, fromBlock, getKitty, getMyKitties, myKitties, sellKitty, status, bg, breedGroup }
