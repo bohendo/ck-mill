1301ebfeff4c597a23d2526e8635a36947973e37
Bo Henderson <bohende@gmail.com>
2017-12-15 17:37:48 -0800

    Initialize sellKitty function

--- src/index.js
+++ src/index.js
@@ -6,6 +6,39 @@ const myKitties = [291628, 282323, 279505, 258963, 228842, 117491, 113881, 85736
 
 const fromBlock = 4605167
 
+const status = () => {
+  if (eth.syncing) {
+    return `On block ${eth.syncing.currentBlock} (latest is ${eth.syncing.highestBlock-eth.syncing.currentBlock} blocks ahead)`
+  } else {
+    return `On block ${eth.blockNumber} (we're up to date!)`
+  }
+}
+
+const sellKitty = (id, milli) => {
+  personal.unlockAccount(eth.accounts[0])
+  core.createSaleAuction.sendTransaction(
+    id,
+    web3.toWei(milli,'milli'),
+    0,
+    129600,
+    {
+      from: eth.accounts[0],
+      to: core.address,
+      value: 0,
+      gas: 250000,
+    },
+    (error, txhash) => {
+      if (error) {
+        console.error(JSON.stringify(error, null, 2))
+      } else {
+        console.log(JSON.stringify(eth.getTransaction(txhash), null, 2))
+      }
+    }
+  )
+  personal.lockAccount(eth.accounts[0]) // don't accidently sell twice!
+  return status()
+}
+
 const getMyKitties = (addr) => {
   const myKitties = []
   let max = core.totalSupply.call()
@@ -74,4 +107,4 @@ const getKitty = (id) => {
   return kitty
 }
 
-export { core, sale, sire, fromBlock, getKitty, getMyKitties, myKitties }
+export { core, sale, sire, fromBlock, getKitty, getMyKitties, myKitties, sellKitty, status }
