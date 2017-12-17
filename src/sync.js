41e637f92b5c7e99d4afcf06f41043b9ec8d9b28
Bo Henderson <bohende@gmail.com>
2017-12-17 12:15:19 -0800

    Initialize first event syncer

--- src/sync.js
+++ src/sync.js
@@ -3,12 +3,6 @@ import db from './db/'
 
 const printq = true
 
-// Activate event listeners
-web3.eth.getBlock('latest').then(res => {
-  console.log(`Starting event watchers from block ${res.number}`)
-  syncAuctionSuccessful(res.number)
-})
-
 const syncAuctionCreated = (from) => {
   db.query(`CREATE TABLE IF NOT EXISTS saleAuctionCreated (
     txhash     CHAR(66)    PRIMARY KEY,
@@ -18,6 +12,7 @@ const syncAuctionCreated = (from) => {
     endprice   NUMERIC(78) NOT NULL,
     duration   BIGINT      NOT NULL);`)
 }
+
 
 // Define event listeners
 const syncAuctionSuccessful = (from) => {
@@ -29,6 +24,17 @@ const syncAuctionSuccessful = (from) => {
     price      NUMERIC(78) NOT NULL,
     winner     CHAR(42)    NOT NULL);`)
 
+  const saveAuctionSuccessful = (event) => {
+    let q = `INSERT INTO saleAuctionSuccessful VALUES (
+      '${event.transactionHash}',
+       ${event.blockNumber},
+       ${event.returnValues[0]},
+       ${event.returnValues[1]},
+      '${event.returnValues[2]}');`
+    if (printq) { console.log(q) }
+    db.query(q).catch(console.error)
+  }
+
   // get current/future events
   ck.sale.events.AuctionSuccessful({ fromBlock: from }, (err, res) => {
     if (err) { console.error(err); process.exit(1) }
@@ -36,22 +42,23 @@ const syncAuctionSuccessful = (from) => {
   })
 
   // get past events
-  ck.sale.getPastEvents('AuctionSuccessful', { fromBlock: from-10, toBlock: from-1 }, (err, res) => {
-    if (err) { console.error(err); process.exit(1) }
-    res.forEach(event=>{
-      saveAuctionSuccessful(event)
+  const pastEventLoop = (i) => {
+    if (i < mn.fromBlock) return ('done')
+    ck.sale.getPastEvents('AuctionSuccessful', { fromBlock: i, toBlock: i }, (err, res) => {
+      if (err) { console.error(err); process.exit(1) }
+      res.forEach(event=>{
+        saveAuctionSuccessful(event)
+      })
     })
-  })
+    // Move on to the next
+    pastEventLoop(i-1)
+  }
+  pastEventLoop(from)
 }
 
-const saveAuctionSuccessful = (event) => {
-  let q = `INSERT INTO saleAuctionSuccessful VALUES (
-    '${event.transactionHash}',
-     ${event.blockNumber},
-     ${event.returnValues[0]},
-     ${event.returnValues[1]},
-    '${event.returnValues[2]}');`
-  if (printq) { console.log(q) }
-  db.query(q).catch(console.error)
-}
+// Activate!
+web3.eth.getBlock('latest').then(res => {
+  console.log(`Starting event watchers from block ${res.number}`)
+  syncAuctionSuccessful(res.number)
+})
 
