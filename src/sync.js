eb320bb59411918ce9157fac83be247583bedfaf
Bo Henderson <bohende@gmail.com>
2017-12-17 18:34:45 -0800

    Generalize auction event syncer to capture all auction events

--- src/sync.js
+++ src/sync.js
@@ -4,71 +4,94 @@ import db from './db/'
 const printq = true
 
 const syncAuctionCreated = (from) => {
-  db.query(`CREATE TABLE IF NOT EXISTS saleAuctionCreated (
-    txhash     CHAR(66)    PRIMARY KEY,
-    blockn     BIGINT      NOT NULL,
-    kittyid    BIGINT      NOT NULL,
-    startprice NUMERIC(78) NOT NULL,
-    endprice   NUMERIC(78) NOT NULL,
-    duration   BIGINT      NOT NULL);`)
 }
 
 
 // Define event listeners
-const syncAuctionSuccessful = (from) => {
+const syncAuctions = (from) => {
 
   // sors for Sale or Sire, equals either 'sale' or 'sire'
   const dbinit = (sors) => {
+    db.query(`CREATE TABLE IF NOT EXISTS ${sors}AuctionCreated (
+      txhash     CHAR(66)    PRIMARY KEY,
+      blockn     BIGINT      NOT NULL,
+      kittyid    BIGINT      NOT NULL,
+      startprice NUMERIC(78) NOT NULL,
+      endprice   NUMERIC(78) NOT NULL,
+      duration   BIGINT      NOT NULL);`)
     db.query(`CREATE TABLE IF NOT EXISTS ${sors}AuctionSuccessful (
       txhash     CHAR(66)    PRIMARY KEY,
       blockn     BIGINT      NOT NULL,
       kittyid    BIGINT      NOT NULL,
       price      NUMERIC(78) NOT NULL,
       winner     CHAR(42)    NOT NULL);`)
+    db.query(`CREATE TABLE IF NOT EXISTS ${sors}AuctionCancelled (
+      txhash     CHAR(66)    PRIMARY KEY,
+      blockn     BIGINT      NOT NULL,
+      kittyid    BIGINT      NOT NULL);`)
   }
   dbinit('sale')
   dbinit('sire')
 
-  const saveAuctionSuccessful = (event, sors) => {
-    let q = `INSERT INTO ${sors}AuctionSuccessful VALUES (
-      '${event.transactionHash}',
-       ${event.blockNumber}, ${event.returnValues[0]}, ${event.returnValues[1]},
-      '${event.returnValues[2]}');`
+  const saveAuction = (auction, sors, event) => {
+    let q = `INSERT INTO ${sors}${event} VALUES (`
+    if (event === 'AuctionCreated') {
+      q += `'${auction.transactionHash}',
+       ${auction.blockNumber}, ${auction.returnValues[0]}, ${auction.returnValues[1]},
+       ${auction.returnValues[2]}, ${auction.returnValues[3]});`
+    } else if (event === 'AuctionSuccessful') {
+      q += `'${auction.transactionHash}',
+       ${auction.blockNumber}, ${auction.returnValues[0]}, ${auction.returnValues[1]},
+      '${auction.returnValues[2]}');`
+    } else if (event === 'AuctionCancelled') {
+      q += `'${auction.transactionHash}',
+       ${auction.blockNumber}, ${auction.returnValues[0]});`
+    }
+
     if (printq) { console.log(q) }
     db.query(q).catch(error=>{
+      // I'll let postgres quietly sort out my duplicate queries for me
       if (error.code !== '23505') { console.error(error) }
     })
   }
 
-  const getAuctionSuccessful = (sors) => {
+  const listen = (from, sors, event) => {
     // get current/future events
-    ck[sors].events.AuctionSuccessful({ fromBlock: from }, (err, res) => {
+    ck[sors].events[event]({ fromBlock: from }, (err, res) => {
       if (err) { console.error(err); process.exit(1) }
-      saveAuctionSuccessful(res, sors)
+      saveAuction(res, sors, event)
     })
   }
-  getAuctionSuccessful('sale')
-  getAuctionSuccessful('sire')
+  listen(from, 'sale', 'AuctionCreated')
+  listen(from, 'sire', 'AuctionCreated')
+  listen(from, 'sale', 'AuctionSuccessful')
+  listen(from, 'sire', 'AuctionSuccessful')
+  listen(from, 'sale', 'AuctionCancelled')
+  listen(from, 'sire', 'AuctionCancelled')
 
   // get past events | sors for Sale OR Sire
-  const pastEventLoop = (i, sors) => {
+  const remember = (i, sors, event) => {
     if (i < mn.fromBlock) return ('done')
-    ck[sors].getPastEvents('AuctionSuccessful', { fromBlock: i, toBlock: i }, (err, res) => {
+    ck[sors].getPastEvents(event, { fromBlock: i, toBlock: i }, (err, res) => {
       if (err) { console.error(err); process.exit(1) }
-      res.forEach(event=>{
-        saveAuctionSuccessful(event, sors)
+      res.forEach(auction=>{
+        saveAuction(auction, sors, event)
       })
     })
-    // Move on to the next
-    pastEventLoop(i-1, sors)
+    // Move on to the next (give node a hot sec to clear the call stack)
+    setTimeout(()=>{remember(i-1, sors, event)}, 0)
   }
-  pastEventLoop(from, 'sale')
-  pastEventLoop(from, 'sire')
+  remember(from, 'sale', 'AuctionCreated')
+  remember(from, 'sire', 'AuctionCreated')
+  remember(from, 'sale', 'AuctionSuccessful')
+  remember(from, 'sire', 'AuctionSuccessful')
+  remember(from, 'sale', 'AuctionCancelled')
+  remember(from, 'sire', 'AuctionCancelled')
 }
 
 // Activate!
 web3.eth.getBlock('latest').then(res => {
   console.log(`Starting event watchers from block ${res.number}`)
-  syncAuctionSuccessful(res.number)
+  syncAuctions(res.number)
 })
 
