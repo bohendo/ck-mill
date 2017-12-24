022d58c94c964f50b8985d0e9e87bc4f52c25f52
Bo Henderson <bohende@gmail.com>
2017-12-24 11:54:29 -0500

    Stop downloading events from blocks we've already downloaded

--- src/sync.js
+++ src/sync.js
@@ -49,6 +49,7 @@ const syncEvents = () => {
       sireid      BIGINT      NOT NULL,
       cooldownend NUMERIC(78) NOT NULL);`)
 
+    // contract [string] will be one of 'sale', or 'sire'
     const auctionTableInit = (contract) => {
       db.query(`CREATE TABLE IF NOT EXISTS ${contract}AuctionCreated (
         txhash     CHAR(66)    PRIMARY KEY,
@@ -71,13 +72,12 @@ const syncEvents = () => {
     auctionTableInit('sale')
     auctionTableInit('sire')
 
+    // contract [string] will be one of 'core', 'sale', or 'sire'
     // name [string] will be one of 'transfer', 'approval', 'birth', or 'pregnant'
     // data [object] will contain tx receipt and return values from event
     const saveEvent = (contract, name, data) => {
       let table = ''
-      if (contract === 'sale' || contract === 'sire') {
-        table += contract
-      }
+      if (contract === 'sale' || contract === 'sire') { table += contract }
       table += name
 
       let q = `INSERT INTO ${table} VALUES ('${data.transactionHash}', 
@@ -110,14 +110,16 @@ const syncEvents = () => {
         ${data.returnValues[1]}, ${data.returnValues[2]}, ${data.returnValues[3]});`
       }
 
-      if (printq) { console.log(q) }
-      db.query(q).catch(error=>{
+      db.query(q).then(res=>{
+        if (printq) { console.log(q) }
+      }).catch(error=>{
         // I'll let postgres quietly sort out my duplicate queries for me
         if (error.code !== '23505') { console.error(error) }
       })
     }
 
     // fromBlock [int] start listening from this block number
+    // contract [string] will be one of 'core', 'sale', or 'sire'
     // name [string] of event to listen for
     const sync = (fromBlock, contract, name) => {
       // get current/future events
@@ -126,20 +128,40 @@ const syncEvents = () => {
         saveEvent(contract, name, data)
       })
 
-      // i [int] remember past events from block number i
-      // name [string] of event to remember
-      const remember = (i, contract, name) => {
-        if (i < firstBlock) return ('done')
-        ck[contract].getPastEvents(name, { fromBlock: i, toBlock: i }, (err, pastEvents) => {
-          if (err) { console.error(err); process.exit(1) }
-          pastEvents.forEach(data=>{
-            saveEvent(contract, name, data)
-          })
-        })
-        // give node a sec to clear the call stack & give geth a sec to stay synced
-        setTimeout(()=>{remember(i-1, contract, name)}, throttle)
-      }
-      remember(fromBlock, contract, name)
+      let table = ''
+      if (contract === 'sale' || contract === 'sire') { table += contract }
+      table += name
+
+      db.query(`SELECT blockn FROM ${table} ORDER BY blockn;`).then(res=>{
+        // lobn for List Of Block Numbers
+        const lobn = res.rows.map(row => Number(row.blockn))
+
+        // i [int] remember past events from block number i
+        // contract [string] will be one of 'core', 'sale', or 'sire'
+        // name [string] of event to remember
+        const remember = (i, contract, name) => {
+          if (i < firstBlock) return ('done')
+
+          if (lobn.includes(i)) {
+            // give node a sec to clear the call stack, no need to let geth stay synced
+            setTimeout(()=>{remember(i-1, contract, name)}, 1)
+
+          } else {
+            ck[contract].getPastEvents(name, { fromBlock: i, toBlock: i }, (err, pastEvents) => {
+              if (err) { console.error(err); process.exit(1) }
+              pastEvents.forEach(data=>{ saveEvent(contract, name, data) })
+
+              // give node a sec to clear the call stack & give geth a sec to stay synced
+              setTimeout(()=>{remember(i-1, contract, name)}, throttle)
+            })
+          }
+        }
+
+        remember(fromBlock, contract, name)
+
+      }).catch(console.error)
+
+
     }
 
     sync(fromBlock, 'core', 'Transfer')
