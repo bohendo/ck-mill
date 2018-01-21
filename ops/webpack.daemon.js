97341c6091975123761d12f9a20e5c7980309eac
Bo Henderson <bohende@gmail.com>
2018-01-21 14:11:40 -0500

    Add binary loader to webpack.daemon.js

--- ops/webpack.daemon.js
+++ ops/webpack.daemon.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   resolve: {
-    extensions: ['.js', '.json'],
+    extensions: ['.js', '.json', '.node'],
     // scrypt.js says "if target is node, use c++ implementation"
     // but I don't want any c++, let's force the js version to load
     alias: { 'scrypt.js': 'scryptsy' },
@@ -33,6 +33,12 @@ module.exports = {
           options: { presets: ['es2015'], },
         },
         exclude: /node_modules/,
+      },
+      {
+        test: /\.node$/,
+        use: {
+          loader: 'binary-loader',
+        },
       },
     ],
   },
