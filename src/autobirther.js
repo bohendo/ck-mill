ccb3f6b9c0d45aa0e5adbaa51c8c67994c393e7c
Bo Henderson <bohende@gmail.com>
2018-01-24 12:05:13 -0500

    Overhaul autobirther's data collection & parsing

--- src/autobirther.js
+++ src/autobirther.js
@@ -4,167 +4,208 @@ import db from './db/'
 ////////////////////////////////////////
 // Global Variables
 
-// A day/week's worth of blocks
-const DAY = 4*60*24
-const WEEK = 4*60*24*7
 
-// master list of duedates
-// duedates[i] = { block: 40123, due: [20456, 20789] }
-// if kitties 20456 and 20789 are due to give birth at block 40123
-const DUEDATES = []
-
-let TIME = new Date().getTime()/1000
+// Master list of kitties and when they'll be due to give birth
+var DUEDATES
 
 ////////////////////////////////////////
-// Function Definitions
+// Updates an existing (maybe []) duedates array based on arrays of pregnant and birth events
+// pure function, no side effects
+// (duedates, pregos, births) => duedates
+// where
+// - pregos[i] = { matronid: 123, blockn: 460, cooldownend: 464, ... }
+//   if we got a pregnant event for kitty 123 at block 460
+//   and it'll be ready to give birth at block 464
+// - births[i] = { kittyid: 12345, blockn: 46400, ... }
+//   if we got a birth event for kitty 12345 at block 46400
+// - duedates[i] = { kittyid: 123, blockn: 464 }
+//   if kitty123 is pregnant and due on block 464
+const update = (duedates, pregos, births) => {
 
-// pregos[i] = { matronid: 123, blockn: 460, cooldownend: 464 }
-// if we got a pregnant event for kitty 123 at block 460
-// and it'll be ready to give birth at block 464
-// births[i] = { kittyid: 12345, blockn: 46400 }
-// if we got a birth event for kitty 12345 at block 46400
-const updateDueDates = (pregos, births) => {
+  console.log(`${new Date().toISOString()} Updating list of ${duedates.length} duedates according to ${births.length} birth events and ${pregos.length} pregnant events`)
 
-  console.log(`Analyzing ${pregos.length} pregnancies and ${births.length} births...`)
+  pregos.forEach(preg=>{ // for each pregnancy event
 
-  // for each pregnancy event, save the due date to our DUEDATE calendar
-  pregos.forEach(preg=>{
-    const cooldownend = Number(preg.cooldownend)
+    // translate results to proper types
     const matronid = Number(preg.matronid)
+    const cooldownend = Number(preg.cooldownend)
 
-    if (matronid === 278308) { console.log(`found pregnancy for kitty ${matronid} due at block ${cooldownend}`) }
+    let isNew = true // does this kitty have an entry in duedates yet?
+    
+    // search through existing due dates
+    for (let i=0; i<duedates.length; i++) {
 
-    // find the entry in duedates for this cooldownend block
-    let isNew = true
-    for (let i=0; i<DUEDATES.length; i++) {
-      if (DUEDATES[i].block == cooldownend) {
+       // does this matronid already have an entry in duedates?
+      if (matronid === duedates[i].kittyid){
         isNew = false
-        // add this due date if we haven't already
-        if (!DUEDATES[i].due.includes(matronid)) {
-          DUEDATES[i].due.push(matronid)
+        // if this one more recent, overwrite the old one
+        if (cooldownend > duedates[i].blockn) {
+          duedates[i].kittyid = cooldownend
         }
-        break
       }
     }
-    // no due dates for this block yet?
-    // add a square to our calendar and pencil in this kittyid
+
+    // if this kitty doesn't have an entry in duedates yet, add one
     if (isNew) {
-      DUEDATES.push({ block: cooldownend, due: [matronid] })
+      duedates.push({ kittyid: matronid, blockn: cooldownend })
     }
   })
 
-  // remove all due dates that have already resulted in a birth
-  births.forEach(birth=>{
-    const block = Number(birth.blockn)
+  births.forEach(birth=>{ // for each birth event
+
+    // translate results to proper types
     const matronid = Number(birth.matronid)
+    const block = Number(birth.blockn)
 
-    if (matronid === 278308) { console.log(`found birth for kitty ${matronid} at block ${block}`) }
-
-    // for all our due dates..
-    for (let i=0; i<DUEDATES.length; i++) {
-      // If this birth comes after a date at which this kitty was due..
-      if (DUEDATES[i].block <= block && DUEDATES[i].due.includes(matronid)) {
-        // remove this due date
-        DUEDATES[i].due.splice(DUEDATES[i].due.indexOf(matronid), 1)
-      }
-      // If this due date is empty now, get rid of it
-      if (DUEDATES[i].due.length === 0) {
-        DUEDATES.splice(i, 1)
-        i -= 1 // We removed an element from the list so the same index now points to the next element
+    // search through existing due dates
+    for (let i=0; i<duedates.length; i++) {
+      // remove any duedates that were before this kitty's most recent birth
+      if (matronid === duedates[i].kittyid && block >= duedates[i].blockn) {
+        duedates.splice(i, 1)
+        i -= 1 // We removed an element from the list so this i now points to the next element
       }
     }
   })
 
   // Sort due dates so that most recent is on top
-  DUEDATES.sort((a,b) => { return a.block-b.block })
+  duedates.sort((a,b) => { return a.blockn-b.blockn })
 
-  // print the first 10 due dates
-  console.log('Tentative DUEDATES:')
-  for (let i=0; i<10; i++) { console.log(`${JSON.stringify(DUEDATES[i])}`) }
-
-  // how many pregnant kitties did we find?
-  let npreg = 0
-  for (let i=0; i<DUEDATES.length; i++) {
-    npreg += DUEDATES[i].due.length
-  }
-  console.log(`We found ${npreg} pregnant kitties`)
-  
-  core.methods.pregnantKitties().call().then(res=>{
-    console.log(`The blockchain says there are ${JSON.stringify(res)} pregnant kitties`)
-  }).catch(console.error)
-
-  // loop through the kitties we found and figure out which ones shouldn't be here
-  for (let i=0; i<DUEDATES.length; i++) {
-    for (let j=0; j<DUEDATES[i].due.length; j++) {
-      core.methods.getKitty(DUEDATES[i].due[j]).call().then(kitty=>{
-        
-        if (!kitty.isGestating) {
-          console.log(`Kitty ${DUEDATES[i].due[j]} is NOT pregnant but was supposedly due at block ${DUEDATES[i].block}, removing...`)
-          DUEDATES[i].due.splice(j, 1)
-        }
-
-      }).catch(()=>{ console.log(`Error getting kitty ${DUEDATES[i].due[j]}`) })
+  // double check everything if we have too many due dates on our calendar
+  return core.methods.pregnantKitties().call().then(res=>{
+    // too many due dates? double check what we got
+    if (res < duedates.length) {
+      console.log(`${new Date().toISOString()} WARN: we have too many duedates, double checking with the blockchain...`)
+      return doublecheck(duedates)
+    } else {
+      if (res < duedates.length) {
+        console.log(`${new Date().toISOString()} WARN: we're missing ${duedates.length - res} due dates`)
+      }
+      console.log(`${new Date().toISOString()} ${JSON.stringify(duedates.slice(0,3))}`)
+      return duedates
     }
-  }
-
-  // print the first 10 due dates
-  console.log('REAL DUEDATES:')
-  for (let i=0; i<10; i++) { console.log(`${JSON.stringify(DUEDATES[i])}`) }
+  })
 }
 
-const initializeDueDates = (callback) => {
+////////////////////////////////////////
+// Double check each due date & remove any that expired w/out us noticing
+const doublecheck = (duedates) => {
+
+  // loop through the kitties we found and get the status of each
+  const kittyPromises = duedates.map(dd => core.methods.getKitty(dd.kittyid).call())
+
+  // wait for all our kitty data to return
+  return (Promise.all(kittyPromises).then(kitties=>{
+
+    // remove any kitties we found that aren't pregnant anymore
+    for (let i=0; i<kitties.length; i++) {
+
+      // if this kitty isn't pregnant...
+      if (!kitties[i].isGestating) {
+        console.log(`${new Date().toISOString()} WARN: we thought kitty ${duedates[i].kittyid} was pregnant but isGestating=${kitties[i].isGestating}, removing...`)
+
+        // remove this index from both arrays
+        duedates.splice(i, 1)
+        kitties.splice(i, 1)
+        i -= 1 // We removed an element from the list so the same index now points to the next element
+      }
+
+    } // done looping through pregnant kitties, any non-pregnant ones have been removed
+
+    console.log(`${new Date().toISOString()} ${JSON.stringify(duedates.slice(0,3))}`)
+    return (duedates)
+
+  }).catch((err)=>{ console.error(err) }))
+
+}
+
+
+////////////////////////////////////////
+// Pulls old events out of our database and calls update()
+// to populate our duedates calendar
+const init = (listen) => {
   web3.eth.getBlock('latest').then(res=>{
 
     const latest = Number(res.number)
-    console.log(`Starting from latest block: ${latest}`)
+
+    console.log(`${new Date().toISOString()} Initializing history before latest block: ${latest}`)
+
+    const week = 4*60*24*7 // a week's worth of blocks
 
     // get all pregnancy events from the last week with most recent first
     const preg_query = `
       SELECT matronid,blockn,cooldownend
       FROM pregnant
-      WHERE blockn > ${latest-WEEK-1}
+      WHERE blockn > ${latest-week}
       ORDER BY blockn DESC;`
 
     // get only the most recent birth for each kitty this week
     const birth_query = `
       SELECT DISTINCT ON (matronid) matronid,blockn
       FROM birth
-      WHERE blockn > ${latest-WEEK-100}
+      WHERE blockn > ${latest-week}
       ORDER BY matronid DESC, blockn DESC;`
 
     db.query(preg_query).then(pregos => {
-
       db.query(birth_query).then(births => {
 
-        updateDueDates(pregos.rows, births.rows)
-        console.log(`Done initializing due dates in ${(new Date().getTime()/1000)-TIME} seconds`)
-        callback()
+        // update empty erray w historic birth/pregnancy events
+        update([], pregos.rows, births.rows).then(duedates=>{
+          // save result to our global variable
+          DUEDATES = duedates
+          listen()
+        })
 
-      }).catch((error) => {
-        console.error(birth_query, error)
-        process.exit(1)
+      }).catch((error) => { console.error(birth_query, error); process.exit(1) })
+    }).catch((error) => { console.error(preg_query, error); process.exit(1) })
+
+  }).catch((error) => { console.error('web3.eth.getBlock error:', error); process.exit(1) })
+}
+
+
+////////////////////////////////////////
+// Listens for new events and calls updateDueDates()
+// to keep our DUEDATES calendar up-to-date
+const listen = () => {
+
+  // We'll manually get events each time we import a new block
+  web3.eth.subscribe('newBlockHeaders', (err, header) => {
+    if (err) { console.error(err); process.exit(1) }
+    const block = Number(header.number)
+    console.log(`${new Date().toISOString()} --- Imported new block ${block}`)
+
+    // ethprovider's buggy & occasionally skips events and imported blocks
+    // get events from several of the most recent blocks to partially solve this problem
+    core.getPastEvents('Birth', { fromBlock: block-2, toBlock: block }, (err, pastBirths) => {
+      if (err) { console.error(err); process.exit(1) }
+      core.getPastEvents('Pregnant', { fromBlock: block-2, toBlock: block }, (err, pastPregos) => {
+        if (err) { console.error(err); process.exit(1) }
+
+          const births = pastBirths.map(e=>{
+            return ({
+              matronid: Number(e.returnValues[2]),
+              blockn: Number(e.blockNumber),
+            })
+          })
+
+          const pregos = pastPregos.map(e=>{
+            return ({
+              matronid: Number(e.returnValues[1]),
+              blockn: Number(e.blockNumber),
+              cooldownend: Number(e.returnValues[3]),
+            })
+          })
+
+          // Update our global DUEDATES variable.
+          update(DUEDATES, pregos, births).then(result => {
+            DUEDATES = result
+          })
       })
-
-    }).catch((error) => {
-      console.error(preg_query, error)
-      process.exit(1)
     })
 
-  }).catch((error) => {
-    console.error('web3.eth.getBlock error:', error)
-    process.exit(1)
   })
 }
 
-const listen = () => {
-
-  // TODO: add event listeners:
-  // core.events.Birth({fromblock: something }, ()=>{  })
-
-  console.log('thump thump')
-}
-
 ////////////////////////////////////////
-// Execute
-initializeDueDates(listen)
+// Execute!
+init(listen)
 
