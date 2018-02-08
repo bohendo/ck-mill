db1b6d9e48f43351418ea6b75335fbce38ca195e
Bo Henderson <bohende@gmail.com>
2018-02-08 15:10:50 -0800

    Autobirther bug fix

--- src/eth/autobirther.js
+++ src/eth/autobirther.js
@@ -15,41 +15,40 @@ const executioner = (lodd, tx) => {
 
   var tx
   if (lodd.length === 1) {
-    tx = Autobirther.methods.breed1(lodd[0].kittyid, lodd[0].blockn).send(tx)
+    tx = Autobirther.methods.breed1(lodd[0].matronid, lodd[0].blockn).send(tx)
 
   } else if (lodd.length === 2) {
-    tx = Autobirther.methods.breed2(lodd[0].kittyid, lodd[0].blockn,
-                                    lodd[1].kittyid, lodd[1].blockn).send(tx)
+    tx = Autobirther.methods.breed2(lodd[0].matronid, lodd[0].blockn,
+                                    lodd[1].matronid, lodd[1].blockn).send(tx)
 
   } else if (lodd.length === 3) {
-    tx = Autobirther.methods.breed3(lodd[0].kittyid, lodd[0].blockn,
-                                    lodd[1].kittyid, lodd[1].blockn,
-                                    lodd[2].kittyid, lodd[2].blockn).send(tx)
+    tx = Autobirther.methods.breed3(lodd[0].matronid, lodd[0].blockn,
+                                    lodd[1].matronid, lodd[1].blockn,
+                                    lodd[2].matronid, lodd[2].blockn).send(tx)
 
   } else if (lodd.length === 4) {
-    tx = Autobirther.methods.breed4(lodd[0].kittyid, lodd[0].blockn,
-                                    lodd[1].kittyid, lodd[1].blockn,
-                                    lodd[2].kittyid, lodd[2].blockn,
-                                    lodd[3].kittyid, lodd[3].blockn).send(tx)
+    tx = Autobirther.methods.breed4(lodd[0].matronid, lodd[0].blockn,
+                                    lodd[1].matronid, lodd[1].blockn,
+                                    lodd[2].matronid, lodd[2].blockn,
+                                    lodd[3].matronid, lodd[3].blockn).send(tx)
 
   } else if (lodd.length === 5) {
-    tx = Autobirther.methods.breed5(lodd[0].kittyid, lodd[0].blockn,
-                                    lodd[1].kittyid, lodd[1].blockn,
-                                    lodd[2].kittyid, lodd[2].blockn,
-                                    lodd[3].kittyid, lodd[3].blockn,
-                                    lodd[4].kittyid, lodd[4].blockn).send(tx)
+    tx = Autobirther.methods.breed5(lodd[0].matronid, lodd[0].blockn,
+                                    lodd[1].matronid, lodd[1].blockn,
+                                    lodd[2].matronid, lodd[2].blockn,
+                                    lodd[3].matronid, lodd[3].blockn,
+                                    lodd[4].matronid, lodd[4].blockn).send(tx)
   } else {
-    tx = Autobirther.methods.breedMore(lodd.map(dd=>dd.kittyid)).send(tx)
+    tx = Autobirther.methods.breedMore(lodd.map(dd=>dd.matronid)).send(tx)
   }
 
   // What have we done? Log it so the user knows
-  tx
-  .on('transactionHash', (transactionHash) => {
+  tx.on('transactionHash', (transactionHash) => {
     // log sent transaction
     console.log(`${new Date().toISOString()} Sent tx ${transactionHash}`)
   })
 
-  .on('receipt', (receipt) => {
+  tx.on('receipt', (receipt) => {
     let earn = 0
     if (receipt.events["1"]) earn += 8000
     if (receipt.events["3"]) earn += 8000 
@@ -64,7 +63,7 @@ const executioner = (lodd, tx) => {
 // lodd for List Of Due Dates
 const autobirth = (lodd) => {
 
-  var tx = { gasPrice: "50000000000" }
+  var tx = { gasPrice: "15000000000" }
 
   // get our from address
   return web3.eth.getAccounts().then(accounts=>{ tx.from = accounts[0]
